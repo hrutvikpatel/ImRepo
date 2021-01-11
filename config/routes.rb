9b535cc368fb0979e6e0a07733495c34e7863a2e
Hrutvik Patel <hrutvikk@uoguelph.ca>
2021-01-10 20:32:02 -0500

    added dashboard UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'images#index'
+  root to: redirect('/images')
 
   devise_for :users
   devise_for :admins
   
   resources :images, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:index, :show]
 
   namespace :admin do
-    root to: 'images#index'
+    root to: redirect('/images')
     resources :images, only: [:index, :new, :create, :edit, :update, :destroy]
     resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
   end
